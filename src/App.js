c307b081eb936c83ca448d5133a55b103f62cf92
Prachi Sarvankar <prachisarvankar24@gmail.com>
2023-01-23 14:01:05 +0530

    initialize the project and added functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [playersData, updatedata] = useState([]);
+  const [search, updateSearch] = useState("");
+
+  const fetchData = async () => {
+    const res = await fetch("https://api.npoint.io/20c1afef1661881ddc9c");
+    const data = await res.json();
+    const sortedData = data.playerList.sort((a, b) => a.Value - b.Value);
+    updatedata(sortedData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const smallCase = search.toLowerCase();
+  const filteredData = playersData.filter(
+    ({ TName, PFName }) => PFName.toLowerCase().includes(smallCase) || TName.toLowerCase().includes(smallCase)
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-input">
+        <div className="search-text">Search player:</div>
+        <div>
+          <input value={search} onChange={(e) => updateSearch(e.target.value)} />
+        </div>
+      </div>
+      <div className="player-grid">
+        {filteredData.map(({ Id, PFName, SkillDesc, UpComingMatchesList, Value }) => (
+          <div className="player-card" key={`${Id}-${PFName}`}>
+            <img
+              className="player-img"
+              src={`/player-images/${Id}.jpg`}
+              onError={(e) => (e.target.src = "/player-images/person-icon.jpeg")}
+              alt={PFName}
+            />
+            <div>
+              <b>{PFName}</b>
+              <div>{`Price: $ ${Value}`}</div>
+              <div>{`Position: ${SkillDesc}`}</div>
+              <div>{`Upcoming Match: ${UpComingMatchesList[0].CCode} vs ${UpComingMatchesList[0].VsCCode}`}</div>
+              <div>{`Match Time: ${UpComingMatchesList[0].MDate}`}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
